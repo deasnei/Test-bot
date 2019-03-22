502fbd7f36b5bbe1b2653d24b01dd4e1e4b24e1d
deasnei <48837393+deasnei@users.noreply.github.com>
2019-03-22 19:59:34 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,9 +8,3 @@ const warns = JSON.parse(fs.readFileSync('./warns.json'))
  
 client.login("NTU3OTQ3MzQ5NDA2ODQyODkw.D3PxKg.eAcX6maPcDKhHx3-zUudYb7EcVk")
 
-client.on("ready", () =>{
-    console.log("je suis prêt")
-    client.user.setGame("Joue avec Deasney")
-
-})
-
